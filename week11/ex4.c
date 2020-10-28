743febce1ad50d41b933ff92cb25e0872284d3b6
Amir <66668728+Amirka-Kh@users.noreply.github.com>
2020-10-28 23:51:01 +0300

    Create ex4.c

---
+++ week11/ex4.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAXCHAR 1000
+
+int main (int argc, char *argv[]) {
+
+    char str[MAXCHAR];
+    int in, out;
+    char *src, *dst;
+    struct stat statbuf;
+    int mode = 0x0777;
+
+    // open the input file 
+    in = open (argv[1], O_RDONLY);
+    fgets(str, MAXCHAR, in);
+
+    // open/create the output file 
+    out = open (argv[2], O_RDWR | O_CREAT | O_TRUNC, mode );
+
+    // find size of input file 
+    fstat(in, &statbuf);
+
+    // write a dummy byte at the last location
+    for (int i = 0; i < statbuf.st_size; i++) {
+        write(out, str+i, 1);
+    }
+
+    // mmap the input file 
+    src = mmap(0, statbuf.st_size, PROT_READ, MAP_SHARED, in, 0);
+
+
+    // mmap the output file 
+    dst = mmap (0, statbuf.st_size, PROT_READ | PROT_WRITE,
+        MAP_SHARED, out, 0);
+
+    // this copies the input file to the output file 
+    memcpy (dst, src, statbuf.st_size);
+    return 0;
+}
